e12127f4d6a9ca23d22c778b4c490478e0745cfe
Jamison Orten <jamisonorten@gmail.com>
2018-10-22 17:18:07 -0600

    added new scripts
    fixed player death script, added projectile and player shooting scripts.

--- assignment1/Assets/scripts/LevelManager.cs
+++ assignment1/Assets/scripts/LevelManager.cs
@@ -3,8 +3,68 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 
+     public class LevelManager : MonoBehaviour
 
-public class LevelManager : MonoBehaviour {
+     {
+     
+         public GameObject CurrentCheckPoint;
+         public GameObject Player;
+         public Rigidbody2D PlayerRb;
+         public Renderer playersRender;
+     
+         // Particles
+         public GameObject DeathParticle;
+         public GameObject RespawnParticle;
+         //Respawn Delay
+         public float RespawnDelay;
+         //Point Penalty on Death
+         public int PointPenaltyOnDeath;
+         //Store Gravity Value
+         private float GravityStore;
+         //Use this for initialization
+         void Start()
+         {
+             PlayerRb = GetComponent<Rigidbody2D>();
+             playersRender.GetComponent<Renderer>();
+         }
+     
+         public void RespawnPlayer()
+         {
+             StartCoroutine("RespawnPlayerCo");
+         }
+     
+         public IEnumerator RespawnPlayerCo()
+         {
+             //Hide Player
+             //Player.isKinematic = false;
+             playersRender.enabled = false;
+     
+             //Generate Death Particle
+             Instantiate(DeathParticle, Player.transform.position, Player.transform.rotation);
+             //Point Penalty
+           //  ScoreManager.AddPoints(-PointPenaltyOnDeath);
+             //Show player
+             //Player.isKinematic = true;
+            
+             //Gravity Reset
+             GravityStore = PlayerRb.gravityScale;
+             PlayerRb.gravityScale = 0f;
+             PlayerRb.velocity = Vector2.zero;
+              
+              
+             yield return new WaitForSeconds(RespawnDelay);
+             //Gravity restore   
+             PlayerRb.gravityScale = GravityStore;
+             //Enable players render
+             playersRender.enabled = true;
+             //Instantiate particles
+             Instantiate(RespawnParticle, CurrentCheckPoint.transform.position, CurrentCheckPoint.transform.rotation);
+             //Respawn player at the current check point
+             Player.transform.position = new Vector2 (CurrentCheckPoint.transform.position.x, CurrentCheckPoint.transform.position.y);
+             Debug.Log("Player Respawned");
+         }
+     }
+/* public class LevelManager : MonoBehaviour {
 
 	public GameObject CurrentCheckPoint;
 	private Rigidbody2D Player;
@@ -34,10 +94,7 @@ public class LevelManager : MonoBehaviour {
 	public IEnumerator RespawnPlayerCo(){
 		//Hide Player
 		//Player.isKinematic = false;
-		if(Player != null)
-		{
 		Player.GetComponent<Renderer> ().enabled = false;
-		}
 		//Generate Death Particle
 		Instantiate (DeathParticle, Player.transform.position, Player.transform.rotation);
 		//Point Penalty
@@ -61,3 +118,4 @@ public class LevelManager : MonoBehaviour {
 		Instantiate (RespawnParticle, CurrentCheckPoint.transform.position, CurrentCheckPoint.transform.rotation);
 	}
 }
+*/
