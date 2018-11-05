c639f2a35ebedc65738119f6576eb586c3837a29
Jamison Orten <jamisonorten@gmail.com>
2018-11-05 16:45:40 -0700

    Added new Script
    added arrays and for each script in homework script folder

---
+++ assignment1/Assets/homework scripts/arrays.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrays : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+		string[] Items;
+		Items = new string[5];
+
+		Item[0] = "Health Potion";
+		Item[1] = "Rope";
+		Item[2] = "Water Skin";
+		Item[3] = "Sword";
+		Item[4] = "Crossbow"
+
+		foreach (string item in Inventory){
+			print("Using" + item);
+	}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
