75207478534f0a14f02b6bd76a817d47cc3ab51a
Jamison Orten <jamisonorten@gmail.com>
2018-11-07 15:56:43 -0700

    Modified array script
    Added 4 extra arrays to array script

--- assignment1/Assets/homework scripts/arrays.cs
+++ assignment1/Assets/homework scripts/arrays.cs
@@ -1,30 +1,36 @@
-﻿/* using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class arrays : MonoBehaviour {
 
+	string[] Inventory = new string[5];
+	int [] Ledger = new int[]{25, 62, 3, 79, 1};
+	float [] Floatarray = new float[]{2.3f, 6.9f, 1.8f, 9.3f};
+	double [] Doublearray = new double[]{7.6d, 3.7d, 2.1d, 8.4d};
+	bool [] Switchstate = new bool[]{false, true, false, false, true};
+
 	// Use this for initialization
 	void Start () {
-		
-		string[] Items;
-		Items = new string[5];
 
-		Item[0] = "Health Potion";
-		Item[1] = "Rope";
-		Item[2] = "Water Skin";
-		Item[3] = "Sword";
-		Item[4] = "Crossbow"
+		Inventory[0] = "Health Potion";
+		Inventory[1] = "Rope";
+		Inventory[2] = "Water Skin";
+		Inventory[3] = "Sword";
+		Inventory[4] = "Crossbow";
 
 		foreach (string item in Inventory){
 			print("Using" + item);
 		}
 
+
 	}
+
+	
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
 }
-*/
+
